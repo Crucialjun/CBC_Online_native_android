9ac17788de1a004d979048ad6f87c01806629b3f
Crucialjun <nich.otieno@gmail.com>
2022-08-03 00:00:28 +0300

    onboarding data

--- app/src/main/java/com/example/cbconlinenative/adapters/OnboardingViewpagerAdapter.kt
+++ app/src/main/java/com/example/cbconlinenative/adapters/OnboardingViewpagerAdapter.kt
@@ -4,40 +4,55 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cbconlinenative.R
 import com.example.cbconlinenative.databinding.OnboardingViewpagerLayoutBinding
 import com.example.cbconlinenative.models.OnboardingViewpageriItem
+import com.example.cbconlinenative.utils.getJsonDataFromAsset
 import com.google.gson.GsonBuilder
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromJsonElement
+
 
 class OnboardingViewpagerAdapter(context: Context) :
     RecyclerView.Adapter<OnboardingViewpagerAdapter.ViewHolder>() {
 
-    private val mContext = context
+    val mContext = context
 
+    val itemsJson = getJsonDataFromAsset(mContext,"onboarding_items.json")
     val gson = GsonBuilder().create()
-    val listofItems = gson.fromJson(mContext.assets.open("onboarding_items.json").toString(), Array<OnboardingViewpageriItem>::class.java).toList()
+    private val listofItems: List<OnboardingViewpageriItem> =
+        gson.fromJson(
+        itemsJson,
+        Array<OnboardingViewpageriItem>::class.java
+    ).toList()
 
 
-
-    inner class ViewHolder(private val binding :OnboardingViewpagerLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: OnboardingViewpagerLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(onboardingItems: OnboardingViewpageriItem) {
             with(binding) {
                 viewpagerTitle.text = onboardingItems.title
                 viewpagerDescription.text = onboardingItems.description
+                viewpagerImage.apply {
+                    val imageName = onboardingItems.imageUrl
+                    val id = resources.getIdentifier(imageName,"drawable",mContext.packageName)
+                    setBackgroundResource(id)
+                }
             }
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = OnboardingViewpagerLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = OnboardingViewpagerLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
 
         return ViewHolder(
-           view
+            view
         )
     }
 
--- app/src/main/java/com/example/cbconlinenative/models/OnboardingViewpageriItem.kt
+++ app/src/main/java/com/example/cbconlinenative/models/OnboardingViewpageriItem.kt
@@ -1,10 +1,8 @@
 package com.example.cbconlinenative.models
 
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class OnboardingViewpageriItem(
     val title : String,
     val description : String,
-    val assetName : String
+    val imageUrl : String
 )
